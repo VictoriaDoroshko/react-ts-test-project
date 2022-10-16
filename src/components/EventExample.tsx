65d392953e4c0b66bbc22217c874a19f21c18c52
Vika <viktoriav.aero@gmail.com>
2022-10-16 22:52:21 +0300

    used ref on input

--- src/components/EventExample.tsx
+++ src/components/EventExample.tsx
@@ -1,16 +1,16 @@
-import React, {FC} from 'react';
+import React, {FC, useRef} from 'react';
 import { useState } from 'react';
-
 
 const EventExample: FC = () => {
     const [value, setValue] = useState<string>('')
     const [isDrag, setIsDrag] = useState<boolean>(false)
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value)
     }
     const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log(value)
+        console.log(inputRef.current?.value)
     }
 
     const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
@@ -35,7 +35,8 @@ const EventExample: FC = () => {
 
     return (
         <div>
-            <input value={value} onChange={changeHandler} type="text" />
+            <input value={value} onChange={changeHandler} type="text" placeholder='управляемый' />
+            <input ref={inputRef} type="text" placeholder='неуправляемый' />
             <button onClick={clickHandler}>ggggg</button>
             <div onDrag={dragHandler} draggable style={{width: 200, height: 200, background: 'red'}}></div>
             <div onDrop={dropHandler} onDragLeave={leaveHandler} onDragOver={dragWithPreventHandler} style={{width: 200, height: 200, background: isDrag ? 'blue' : 'red', marginTop: 15}}></div>
