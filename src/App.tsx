b36beff19d9e3f3e1b6edce274218bceda30350d
Vika <viktoriav.aero@gmail.com>
2022-10-17 16:50:13 +0300

    deleted comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,7 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
-import {BrowserRouter, NavLink, Route} from'react-router-dom';
-// import axios from 'axios';
+import {BrowserRouter, NavLink} from'react-router-dom';
+import {Route} from 'react-router'
+
 import UsersPage from './components/UsersPage';
 import TodosPage from './components/TodosPage';
 import UserItemPage from './components/UserItemPage';
@@ -42,18 +16,23 @@ const App = () => {
           <NavLink to='/users'>Пользователи</NavLink>
           <NavLink to='/todos'>Список дел</NavLink>
         </div>
+
         <Route path={'/users'} exact>
             <UsersPage/>
         </Route>
+
         <Route path={'/todos'} exact>
             <TodosPage/>
         </Route>
+
         <Route path={'/user/:id'}>
           <UserItemPage />
         </Route>
+
         <Route path={'/todo/:id'}>
           <TodoItemPage />
         </Route>
+
       </div>
     </BrowserRouter>
   );
